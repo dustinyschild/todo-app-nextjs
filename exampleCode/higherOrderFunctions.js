bd3e8b1600d4e6cf36c9259da374f96e381ada52
unknown <cyelland@williamsburg.k12.ia.us>
2023-10-20 22:34:44 -0500

    styling pages

---
+++ exampleCode/higherOrderFunctions.js
@@ -0,0 +1,22 @@
+blocks = [1, 2, 3, 4, 5]
+
+// 
+const multiplyBy2 = blocks.map((num) => {
+    return num * 2;
+})
+// output => 2, 4, 6, 8, 10
+
+// functions the same as: 
+
+loopMultiplyBy2 = []
+for (let i = 0; i <= blocks.length; i++) {
+    loopMultiplyBy2.push(blocks[i] * 2)
+}
+
+// returns all the 'true' values
+blocks.filter((block) => {
+    if (block === 'wheat') {
+        
+    }
+})
+
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
-
+import styles from "../styles/Signin.module.css"
 export default function SignUpPage() {
-  return <SignUp />;
-}
+  return (<div className= {styles.signInContainer}> <SignUp />
+</div>
+)}
